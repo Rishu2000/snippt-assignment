8e4925612ee0eb13dc7467e1bf6fc1d42cc796aa
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-08-12 08:10:08 +0530

    Added a get method.

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 4001;
 
+app.get('/',(req,res) => {
+    res.json("you are in the root folder.");
+})
+
 app.listen(port, () => {
     console.log('server is listening on port ' + port);
 });
