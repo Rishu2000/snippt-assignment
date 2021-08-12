01ceb20eb0cdc80b6d596f8ec51411fd1555f4e6
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-08-12 09:04:03 +0530

    Implemented a post method.

--- route/Login.js
+++ route/Login.js
@@ -4,5 +4,8 @@ const login = express();
 login.get('/',(req,res) => {
     res.json('You are in login module.');
 })
+login.post('/',(req,res) => {
+    res.json(req.body);
+})
 
 module.exports = login;
