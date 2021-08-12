dcb301d333d686160f1bb2ed28a0b165fb4667d2
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-08-12 14:00:33 +0530

    welcome post method is completed.

--- route/Welcome.js
+++ route/Welcome.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const welcome = express();
 
+const slotData = [];
+
 welcome.get('/', (req, res) => {
-    res.json("Welcome module");
+    res.json(slotData);
+})
+
+welcome.post('/', (req, res) => {
+    const {username,day,time} = req.body;
+    if(!day || !time) {
+        res.staus(400).json({
+            Success:false,
+            Message:"Please enter both day and time"
+        })
+    }else{
+        if(slotData.find(item => item.username === username && !item.permission)){
+            res.status(406).json({
+                Success:false,
+                Message:"Let alumni grant you previous slot permission then after you can register new slot"
+            })
+        }else{
+            slotData.push({username,day,time,permission:false});
+            res.status(201).json({
+                Success:true,
+                Message:"Created"
+            })
+        }
+    }
 })
 
 module.exports = welcome;
