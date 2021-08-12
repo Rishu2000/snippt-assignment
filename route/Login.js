920caded38e18f7136002a73eca672b71e783aec
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-08-12 09:41:59 +0530

    Implemented custom middleware.

--- route/Login.js
+++ route/Login.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const login = express();
 
+login.use((req,res,next) => {
+    console.log("grgr");
+    next();
+})
+
 login.get('/',(req,res) => {
     res.json('You are in login module.');
 })
