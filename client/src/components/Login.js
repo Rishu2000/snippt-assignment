3be6c329f6f18a86934f2cb1d5e850b92eb928c8
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-08-12 08:44:44 +0530

    Created the Login form and added handleSubmit function.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,26 @@
 import React from 'react'
 
 const Login = () => {
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submited");
+}
+
     return (
-        <div>
-           Login 
+        <div style={{height:"100vh", display: 'flex', justifyContent: 'center', paddingTop:60}}>
+           <form onSubmit={handleSubmit} style={{width:'400px'}}>
+            <div className="mb-3">
+                    <label className="form-label">Username</label>
+                    <input type="text" className="form-control"/>
+                    <div className="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Password</label>
+                    <input type="password" className="form-control"/>
+                </div>
+                <button className="btn btn-primary" type="submit">Login</button>
+           </form> 
         </div>
     )
 }
