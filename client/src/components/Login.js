7c78b6a4d4486db114b9230b29d344b464b13459
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-08-12 08:55:45 +0530

    Created username and password state and take the input from Form.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,23 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Login = () => {
 
+const [username, setUsername] = useState(null);
+const [password, setPassword] = useState(null);
+
 const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("submited");
+    console.log("username = " +username+" password = " +password);
 }
 
     return (
         <div style={{height:"100vh", display: 'flex', justifyContent: 'center', paddingTop:60}}>
            <form onSubmit={handleSubmit} style={{width:'400px'}}>
-            <div className="mb-3">
+                <div className="mb-3">
                     <label className="form-label">Username</label>
-                    <input type="text" className="form-control"/>
+                    <input type="text" className="form-control" onChange={(e) => setUsername(e.target.value)}/>
                     <div className="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div className="mb-3">
                     <label className="form-label">Password</label>
-                    <input type="password" className="form-control"/>
+                    <input type="password" className="form-control" onChange={(e) => setPassword(e.target.value)}/>
                 </div>
                 <button className="btn btn-primary" type="submit">Login</button>
            </form> 
