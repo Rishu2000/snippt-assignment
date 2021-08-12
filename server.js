06403a1a1bcbb5b9946d5ee46f00457ee57aac79
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-08-12 09:02:01 +0530

    Implemented body parser middleware.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const login = require('./route/Login');
 const port = 4001;
 
+app.use(express.json());
 app.use('/login',login);
 app.get('/',(req,res) => {
     res.json("you are in the root folder");
